d17da7d6a7e07480c608d380d2b6d2c4e8eceaf6
20agray-creator <20agray@bishophatfield.herts.sch.uk>
2026-03-04 11:11:36 +0000

    Update work.js

--- work.js
+++ work.js
@@ -1,1 +1,20 @@
+<html>
+<body>
+<h1>JavaScript Functions</h1>
+<h2>The return Statement</h2>
+<p>A function that performs a calculation and returns the result:</p>
 
+<p id="demo"></p>
+
+<script>
+function myFunction(a, b) {
+  return a * b;
+}
+
+let x = myFunction(4, 3);
+
+document.getElementById("demo").innerHTML = "The product is " + x;
+</script>
+
+</body>
+</html>
